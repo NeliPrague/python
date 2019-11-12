6aa2bec94c864d5b5e6fd3659b0b2d559b9c0d80
NeliPrague <nelly.c@centrum.cz>
2019-11-12 18:46:34 +0100

    Looping in Python

---
+++ art.py
@@ -0,0 +1,11 @@
+# With a for loop
+for x in range(3):
+	for num in range(1,11): 
+		print("*" * num)
+
+# With a while loop
+times = 1
+while times < 11:
+	print("*" * times)
+	times += 1
+
---
+++ repeater.py
@@ -0,0 +1,7 @@
+times = input("How many times do I have to tell you? ")
+times = int(times)
+
+# Simplest Version
+for time in range(times):
+	print("CLEAN UP YOUR ROOM")
+
